bb6efd5c94f46ee97bbfeb87476cec46fcbd1553
kingpin27 <anishchaudhary2706@gmail.com>
2025-08-29 12:04:58 +0530

    q1 solved

--- user_code.h
+++ user_code.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <unordered_map>
 #include <iostream>
 #include <algorithm>
 
@@ -17,8 +18,25 @@ Complete this function
 
 vector<vector<int>> question_one(const vector<vector<int>>& parcels) {
     // TODO: Implement function
-    vector<vector<int>> ans;
-    return ans;
+    unordered_map<int,int> id_to_weight_map;
+    // O(n) implementation using hashmap
+    for(auto parcel: parcels) {
+        int parcel_id=parcel[0];
+        int parcel_weight=parcel[1];
+        if(id_to_weight_map.find(parcel_id) != id_to_weight_map.end()) {
+            id_to_weight_map[parcel_id] = min(id_to_weight_map[parcel_id], parcel_weight);
+        }
+        else {
+            id_to_weight_map[parcel_id]=parcel_weight;
+        }
+    }
+    vector<vector<int>> deduplicated_parcels;
+    for(auto id_to_weight_element: id_to_weight_map) {
+        int parcel_id=id_to_weight_element.first;
+        int parcel_weight=id_to_weight_element.second;
+        deduplicated_parcels.push_back({parcel_id, parcel_weight});
+    }
+    return deduplicated_parcels;
 }
 
 
